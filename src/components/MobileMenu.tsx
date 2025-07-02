4c0768e0fcafb2a1a0b002042ddefc7b6a918b58
Richard Nemeth <nemethricsi@gmail.com>
2025-07-02 03:20:31 +0200

    adjust mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -19,7 +19,8 @@ export default function MobileMenu() {
   }, [isOpen]);
 
   const closeMenuWhenReachedTop = () => {
-    if (window.scrollY === 0) {
+    // when start scrolling, close the menu
+    if (window.scrollY > 0) {
       setIsOpen(false);
     }
   };
@@ -37,21 +38,21 @@ export default function MobileMenu() {
       <button
         aria-label="Toggle menu"
         onClick={() => setIsOpen(!isOpen)}
-        className="fixed top-6 right-6 z-50 flex h-10 w-10 flex-col items-center justify-center gap-1 rounded-md border border-[#fefefe]/50 bg-black p-1 text-[#fefefe] shadow-lg sm:hidden"
+        className="fixed top-6 right-6 z-50 flex h-12 w-12 cursor-pointer flex-col items-center justify-center gap-1 rounded-full bg-[#fefefe] p-1 text-[#25147B] shadow-lg transition-all sm:hidden"
       >
         {/* Hamburger lines */}
         <motion.span
           animate={isOpen ? { rotate: 45, y: 6 } : { rotate: 0, y: 0 }}
-          className="block h-0.5 w-6 origin-center bg-white"
+          className="block h-0.5 w-6 origin-center bg-[#25147B]"
         />
         <motion.span
           animate={isOpen ? { opacity: 0 } : { opacity: 1 }}
           transition={{ duration: 0.1 }}
-          className="block h-0.5 w-6 bg-white"
+          className="block h-0.5 w-6 bg-[#25147B]"
         />
         <motion.span
           animate={isOpen ? { rotate: -45, y: -6 } : { rotate: 0, y: 0 }}
-          className="block h-0.5 w-6 origin-center bg-white"
+          className="block h-0.5 w-6 origin-center bg-[#25147B]"
         />
       </button>
 
@@ -76,7 +77,7 @@ export default function MobileMenu() {
             animate={{ x: 0 }}
             exit={{ x: '-100%' }}
             transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-            className="fixed top-0 left-0 z-50 flex h-full w-[80%] flex-col bg-gray-900 p-6 text-white shadow-lg md:hidden"
+            className="fixed top-0 left-0 z-50 flex h-full w-[75%] flex-col bg-[#25147B]/75 p-6 text-white shadow-lg backdrop-blur-sm md:hidden"
           >
             <a
               href="#gigs"
