4dbfe1c90344e7186bcb2ef245dc77d5c83d84e8
Richard Nemeth <nemethricsi@gmail.com>
2025-07-02 23:04:24 +0200

    add media section

---
+++ src/components/EmbedBandcamp.tsx
@@ -0,0 +1,17 @@
+export const EmbedBandcamp = () => {
+  return (
+    <iframe
+      title="embed-thekeeymen-bandcamp"
+      className="max-w-[700px]"
+      style={{
+        border: 0,
+        width: '100%',
+        height: 450,
+        display: 'block',
+        margin: '1rem 0',
+      }}
+      src="https://bandcamp.com/EmbeddedPlayer/album=2896876080/size=large/bgcol=ffffff/linkcol=7137dc/artwork=small/transparent=true/"
+      seamless
+    ></iframe>
+  );
+};
