2788640d1ac5c2d479ab9e10c72df5241e148ca9
Richard Nemeth <nemethricsi@gmail.com>
2025-07-04 09:51:59 +0200

    reduce hero section height on mobile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 export const HeroSection = ({ children }: { children: React.ReactNode }) => {
   return (
     <section
-      className="relative flex h-screen w-full flex-col items-start justify-end bg-cover bg-center bg-no-repeat px-3 py-6 text-white bg-blend-lighten md:p-4 md:py-14"
+      className="relative -mb-20 flex h-[calc(100vh-300px)] w-full flex-col items-start justify-end bg-cover bg-center bg-no-repeat px-3 py-6 text-white bg-blend-lighten sm:h-screen md:p-4 md:py-14"
       style={{
         backgroundImage: `linear-gradient(to bottom, #25147B, #8E43A5), url('/images/hero_2_blur_3.png')`,
       }}
