827e3cbc5027a5d65aebc42853b8c28c15e2b6e5
Richard Nemeth <nemethricsi@gmail.com>
2025-07-09 20:16:47 +0200

    change hero image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,9 @@
 export const HeroSection = ({ children }: { children: React.ReactNode }) => {
   return (
     <section
-      className="relative flex h-[calc(100vh-320px)] w-full flex-col items-start justify-end border bg-cover bg-center bg-no-repeat px-3 py-6 text-white bg-blend-lighten sm:h-screen md:p-4 md:py-14"
+      className="relative flex h-[calc(100vh-320px)] w-full flex-col items-start justify-end bg-cover bg-center bg-no-repeat px-3 py-6 text-white bg-blend-lighten sm:h-screen md:p-4 md:py-14"
       style={{
-        backgroundImage: `linear-gradient(to bottom, #25147B, #8E43A5), url('/images/hero.png')`,
+        backgroundImage: `linear-gradient(to bottom, #25147B, #8E43A5), url('/images/hero-blur-3.png')`,
       }}
     >
       {children}
