c6b53c8a214b485177f41c7c6443916c40a54966
Richard Nemeth <nemethricsi@gmail.com>
2025-07-14 08:40:27 +0200

    improve form error handling

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { useForm, ValidationError } from '@formspree/react';
 import { LoaderIcon, SendIcon, XIcon } from 'lucide-react';
 import { useEffect, useRef } from 'react';
@@ -33,29 +34,62 @@ export const ContactForm = () => {
       onSubmit={submitForm}
       className="flex w-full flex-col gap-3 sm:mx-auto sm:max-w-lg"
     >
-      <textarea
-        name="message"
-        rows={5}
-        required
-        className="w-full resize-none border border-[#fefefe]/50 p-4 text-base font-medium focus:bg-[#fefefe]/5 focus:outline-2 focus:outline-offset-3 focus:outline-[#fefefe]"
-        placeholder="Type your message here..."
-      />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <input
-        name="email"
-        type="email"
-        required
-        className="w-full border border-[#fefefe]/50 p-4 text-base font-medium outline-[#fefefe] focus:bg-[#fefefe]/5 focus:outline-2 focus:outline-offset-3"
-        placeholder="Email"
-      />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <input
-        name="phone"
-        type="tel"
-        className="w-full border border-[#fefefe]/50 p-4 text-base font-medium outline-[#fefefe] focus:bg-[#fefefe]/5 focus:outline-2 focus:outline-offset-3"
-        placeholder="Phone number (optional)"
-      />
-      <ValidationError prefix="Phone" field="phone" errors={state.errors} />
+      <div className="flex flex-col gap-1">
+        <textarea
+          name="message"
+          rows={5}
+          required
+          className={cn(
+            'w-full resize-none border border-[#fefefe]/50 p-4 text-base font-medium focus:bg-[#fefefe]/5 focus:outline-2 focus:outline-offset-3 focus:outline-[#fefefe]',
+            (state.errors?.getFieldErrors('message').length || 0 > 0) &&
+              'outline-2 outline-offset-2 outline-rose-300',
+          )}
+          placeholder="Type your message here..."
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+          className="w-fit bg-rose-100 px-2 py-1 text-sm text-rose-500"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <input
+          name="email"
+          type="email"
+          required
+          className={cn(
+            'w-full border border-[#fefefe]/50 p-4 text-base font-medium outline-[#fefefe] focus:bg-[#fefefe]/5 focus:outline-2 focus:outline-offset-3',
+            (state.errors?.getFieldErrors('email').length || 0 > 0) &&
+              'outline-2 outline-offset-2 outline-rose-300',
+          )}
+          placeholder="Email"
+        />
+        <ValidationError
+          prefix="Email"
+          field="email"
+          errors={state.errors}
+          className="w-fit bg-rose-100 px-2 py-1 text-sm text-rose-500"
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <input
+          name="phone"
+          type="tel"
+          className={cn(
+            'w-full border border-[#fefefe]/50 p-4 text-base font-medium outline-[#fefefe] focus:bg-[#fefefe]/5 focus:outline-2 focus:outline-offset-3',
+            (state.errors?.getFieldErrors('phone').length || 0 > 0) &&
+              'outline-2 outline-offset-2 outline-rose-300',
+          )}
+          placeholder="Phone number (optional)"
+        />
+        <ValidationError
+          prefix="Phone"
+          field="phone"
+          errors={state.errors}
+          className="w-fit bg-rose-100 px-2 py-1 text-sm text-rose-500"
+        />
+      </div>
       <button
         type="submit"
         disabled={state.submitting}
