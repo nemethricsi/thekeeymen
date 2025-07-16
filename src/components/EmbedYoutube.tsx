95a204be88ad8ea5df176509007e36ea2a225d93
Richard Nemeth <nemethricsi@gmail.com>
2025-07-16 17:15:11 +0200

    muted playing video

--- src/components/EmbedYoutube.tsx
+++ src/components/EmbedYoutube.tsx
@@ -5,7 +5,14 @@ import ReactPlayer from 'react-player';
 export const EmbedYoutube = ({ src }: { src: string }) => {
   return (
     <div className="aspect-video w-full overflow-hidden rounded-lg">
-      <ReactPlayer src={src} width="100%" height="100%" controls />
+      <ReactPlayer
+        src={src}
+        width="100%"
+        height="100%"
+        controls
+        playing
+        muted
+      />
     </div>
   );
 };
