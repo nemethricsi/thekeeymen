a21853fab7c547c30558fe0bfaa1e1e68cb3b262
Richard Nemeth <nemethricsi@gmail.com>
2025-07-16 11:23:11 +0200

    make useLocaleSwitcher respect url hash (partly)

--- src/hooks/useLocaleSwitcher.ts
+++ src/hooks/useLocaleSwitcher.ts
@@ -1,16 +1,25 @@
 import { getLocaleFromPathname } from '@/lib/utils';
 import { usePathname } from 'next/navigation';
 import { type Locale, i18n } from '@/i18n-config';
+import { useEffect, useState } from 'react';
 
 export const useLocaleSwitcher = () => {
   const pathname = usePathname();
+  const [hash, setHash] = useState<string>('');
   const currentLocale = getLocaleFromPathname(pathname);
+
+  useEffect(() => {
+    setHash(window.location.hash);
+  }, []);
 
   const redirectedPathname = (locale: Locale) => {
     if (!pathname) return '/';
+
     const segments = pathname.split('/');
     segments[1] = locale;
-    return segments.join('/');
+    const newPath = segments.join('/');
+
+    return hash ? `${newPath}${hash}` : newPath;
   };
 
   const isHomePage = i18n.locales
