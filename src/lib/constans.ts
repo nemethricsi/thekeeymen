4f0fc2cb067a75c10592c11de2c2da77b2fc8ff4
Richard Nemeth <nemethricsi@gmail.com>
2025-08-12 14:17:50 +0200

    add MAILERLITE_GROUPS

--- src/lib/constans.ts
+++ src/lib/constans.ts
@@ -1,1 +1,7 @@
 export const baseURL = 'https://www.thekeeymen.com';
+
+// https://dashboard.mailerlite.com/integrations/api
+export const MAILERLITE_GROUPS = {
+  HUNGARIAN: '162376964666033987',
+  ENGLISH: '162525461822309410',
+};
