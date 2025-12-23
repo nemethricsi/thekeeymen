3b2aad9cd9aa5f5392f6ce6b7135ebe3b3fd5bb7
Richard Nemeth <nemethricsi@gmail.com>
2025-12-23 22:11:09 +0100

    ignore serverToken and browserToken

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -11,4 +11,21 @@ export const { sanityFetch, SanityLive } = defineLive({
     apiVersion: 'vX',
     ignoreBrowserTokenWarning: true,
   }),
+  /**
+   * No `serverToken` provided to `defineLive`.
+   * This means that only published content will be fetched
+   * and respond to live events. You can silence this warning
+   * by setting `serverToken: false`.
+   */
+  serverToken: undefined,
+
+  /**
+   * No `browserToken` provided to `defineLive`.
+   * This means that live previewing drafts will only work
+   * when using the Presentation Tool in your Sanity Studio.
+   * To support live previewing drafts stand-alone, provide a `browserToken`.
+   * It is shared with the browser so it should only have Viewer rights or lower.
+   * You can silence this warning by setting `browserToken: false
+   */
+  browserToken: undefined,
 });
